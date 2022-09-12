36808a94dc83a146b6154c3cea9956cde5e74cc7
dipudan <dipudan@gmail.com>
2022-09-12 09:53:09 +0530

    Updated data

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -5,7 +5,7 @@ const ContactCard = (props) => {
 
     return(
         <div className="item">
-            <div className="content">
+            <div className="content" id="abc">
                 <div className="header">{name}</div>
                 <div>{email}</div>
             </div>
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -6,8 +6,8 @@ const ContactList = (props) => {
     const renderContactList = props.contacts.map((contact) => {
         return(
             <div className="item">
-                <div className="content">
-                    <div className="header">{contact.name}</div>
+                <div className="content" id="abc">
+                    <div className="header">{contact.name} </div>
                     <div>{contact.email}</div>
                 </div>
             </div>
