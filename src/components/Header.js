69cca3251f64afddfcb1a2b35a6cd938d661a182
dipudan <dipudan@gmail.com>
2022-09-29 06:55:05 +0530

    Updated Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="ui fixed menu" id="abgc">
             <div className="ui container center">
-                <h2>Contact Managers</h2>
+                <h1>Contact Managers</h1>
             </div>
         </div>
     );
